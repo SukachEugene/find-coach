c66b6a3a61890d247f7d36211b47a12f420d0114
UJ <sukach.eugene@gmail.com>
2024-04-24 17:08:20 +0300

    coaches module is created

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,16 @@ import CoachesModule from './modules/coaches/index.js'
 const store = createStore({
     modules: {
         coaches: CoachesModule
+    },
+    state() {
+        return {
+            userId: 'c3'
+        }
+    },
+    getters: {
+        userId(state) {
+            return state.userId;
+        }
     }
 });
 
