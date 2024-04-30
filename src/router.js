0ab538fbb3b3a3d7c67a8b056f42d12f35db6988
UJ <sukach.eugene@gmail.com>
2024-04-30 19:35:10 +0300

    async components optimization is provided

--- src/router.js
+++ src/router.js
@@ -1,14 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+// import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsRecieved from './pages/requests/RequestsRecieved.vue';
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import ContactCoach from './pages/requests/ContactCoach.vue';
+// import RequestsRecieved from './pages/requests/RequestsRecieved.vue';
 import NotFound from './pages/NotFound.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
 
 import store from './store/index.js';
+
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue');
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const RequestsRecieved = () => import('./pages/requests/RequestsRecieved.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
 
 const router = createRouter({
     history: createWebHistory(),
@@ -30,7 +36,7 @@ const router = createRouter({
     ]
 });
 
-router.beforeEach(function(to, _, next) {
+router.beforeEach(function (to, _, next) {
     if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
         next('/auth');
     } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
